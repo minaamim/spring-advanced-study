5b7cc097ba4c7eb49f3f22bc14ee17612097d55e
minaamim <minaaa5814@gmail.com>
2022-12-19 18:25:16 +0900

    예제 프로젝트

---
+++ src/main/java/cmc/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,31 @@
+package cmc.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        //저장 로직
+        if(itemId.equals("ex")) {
+            try {
+                throw new IllegalAccessException("예외 발생!");
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+    }
+
+}
